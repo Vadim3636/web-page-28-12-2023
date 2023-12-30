9a2673a5004c1040bb36474215d87c1f6b367228
vadik <vadimchubinskiy5@gmail.com>
2023-12-30 23:18:45 +0200

    fixed bugs

--- scripts/cards/cards.js
+++ scripts/cards/cards.js
@@ -10,48 +10,64 @@ async function main() {
 
     let elems = document.querySelectorAll("cards ul li");
     elems[selected_card].querySelector("article").style.filter = "grayscale(0)";
-    let excess = elems[0].offsetLeft;
     let cards_ul = document.querySelector("cards ul");
     cards_ul.onscrollend = (event) => {
         // console.log(event);
-        let i = 0;
-        elems.forEach((element, index) => {
-            
-            // console.log(index, element.offsetLeft);
-            // console.log(index, element.clientWidth);
-            
-            if ( Math.abs((elems[index].offsetLeft+elems[index].clientWidth/2) - excess - cards_ul.scrollLeft) < 
-            Math.abs((elems[i].offsetLeft+elems[i].clientWidth/2) - excess - cards_ul.scrollLeft)) {
-                i = index;
-                // console.log("i: ", i);
-                
-            }
-        });
-        // console.log(":::::", document.querySelector("cards ul").scrollLeft);
-        console.log("selected index:", i);
-        hovered_card = i;
+        calculate_hovered(elems);
 
     };
 
     elems.forEach((element, index) => {
         element.onclick = function() {
+            calculate_hovered(elems);
             if (hovered_card != index) hover_card(index);
             else select_card(index);
+            console.log("asdsd", hovered_card, index);
         };
     });
 }
 main();
 
+function calculate_hovered(elems) {
+    let excess = elems[0].offsetLeft;
+    let cards_ul = document.querySelector("cards ul");
+    let i = 0;
+    elems.forEach((element, index) => {
+            
+        // console.log(index, element.offsetLeft);
+        // console.log(index, element.clientWidth);
+        
+        if ( Math.abs((elems[index].offsetLeft+elems[index].clientWidth/2) - excess - cards_ul.scrollLeft) < 
+        Math.abs((elems[i].offsetLeft+elems[i].clientWidth/2) - excess - cards_ul.scrollLeft)) {
+            i = index;
+            // console.log("i: ", i);
+            
+        }
+    });
+    // console.log(":::::", document.querySelector("cards ul").scrollLeft);
+    console.log("selected index:", i);
+    hovered_card = i;
+    return hovered_card;
+}
+
 function select_card(index) {
+    console.log("select card");
     let elems = document.querySelectorAll("cards ul li");
     if (index + 1 > elems.length || index < 0) {
         console.log("length error");
         return;
     }
+    selected_card = index;
+    update_cards_grayscale();
+}
+
+function update_cards_grayscale() {
+    let elems = document.querySelectorAll("cards ul li");
+
     elems.forEach(element => {
         element.querySelector("article").style.filter = "";
     });
-    elems[index].querySelector("article").style.filter = "grayscale(0)";
+    elems[selected_card].querySelector("article").style.filter = "grayscale(0)";
 }
 
 function hover_card(index, fast) {
